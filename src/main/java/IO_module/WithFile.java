228eeb49e970a3af2f4962b06d4730c06771db7b
Maxim Gorchitskiy <mgorchitskiy@mail.ru>
2025-09-06 20:09:16 +0600

    fix(io): Были переписаны 2 метода
    Основные изменения: Теперь читать можно несколько коэффициентов уравнения - создается eq_ptr для коэффициентов
    - Также в writeFile был упрощен ввод в файл

--- src/main/java/IO_module/WithFile.java
+++ src/main/java/IO_module/WithFile.java
@@ -7,26 +7,50 @@ import java.util.ArrayList;
  * withFile - реализует вывод/ввод через файл
  */
 public class WithFile {
-    public ArrayList<Double> readWriteFile(Reader user_read,Writer user_write,ArrayList<Double> user_Arr)  {
+    public ArrayList<ArrayList<Double>> readFile(Reader user_read)  {
 
-        try (BufferedReader bReader = new BufferedReader(user_read);
-            PrintWriter printWriter = new PrintWriter(user_write))
+        ArrayList<ArrayList<Double>> eq_ptr = new ArrayList<>();
+
+
+        try (BufferedReader bReader = new BufferedReader(user_read))
         {
-            String[] str = bReader.readLine().split(" ");
-            for (int i=0;i<user_Arr.size();i++)
+            String str;
+            String[] parts;
+            double value;
+            while((str=bReader.readLine())!=null)
             {
-                user_Arr.set(i, Double.parseDouble(str[i]));
-                printWriter.write(String.valueOf(user_Arr.get(i)));
-                if(i<user_Arr.size()-1)
+                parts = str.split(" ");
+                ArrayList<Double> ptr = new ArrayList<>();
+                for (int i=0;i<parts.length;i++)
                 {
-                    printWriter.write(" ");
+                    value = Double.parseDouble(parts[i]);
+                    ptr.add(value);
                 }
+                eq_ptr.add(ptr);
             }
+
         }catch (IOException e)
         {
             throw new RuntimeException(e);
         }
-        return user_Arr;
+        return eq_ptr;
+    }
+    public void writeFile(Writer user_write,ArrayList<Double> user_arr)  {
+        try
+        {
+            PrintWriter printWriter = new PrintWriter(user_write);
+            for (int i=0;i<user_arr.size();i++)
+            {
+                printWriter.print(user_arr.get(i));
+                if(i<user_arr.size()-1)
+                {
+                    printWriter.write(" ");
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
