6d05a17a670618b57259ea41a2f30e9c8cfbe95e
Maxim Gorchitskiy <mgorchitskiy@mail.ru>
2025-09-06 19:28:45 +0600

    test(core): в Program попробовал запустить на коэффициентах 1-4-1
    - Итог: -0.2679491924311228
            -3.732050807568877

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,9 +1,15 @@
 import IO_module.*;
 import Math_module.*;
 
+import java.util.ArrayList;
+
 
 public class Program {
     public static void main(String[] args) {
-
+        WithConsole console = new WithConsole();
+        ArrayList<Double> coeffs = console.inputWithConsole();
+        QuadraticSolver roots = new QuadraticSolver(coeffs);
+        ArrayList<Double> result = roots.solve();
+        console.outputWithConsole(result);
     }
 }
