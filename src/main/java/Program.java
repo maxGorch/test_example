9f133a1fcdde054efa4ea0d173972909bc9f01f2
Maxim Gorchitskiy <mgorchitskiy@mail.ru>
2025-09-06 20:12:40 +0600

    test(core): Протестирован на 2 примерах: [1,4,1],[2,3,2] ввод и вывод

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,15 +1,46 @@
 import IO_module.*;
 import Math_module.*;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
 public class Program {
-    public static void main(String[] args) {
-        WithConsole console = new WithConsole();
-        ArrayList<Double> coeffs = console.inputWithConsole();
-        QuadraticSolver roots = new QuadraticSolver(coeffs);
-        ArrayList<Double> result = roots.solve();
-        console.outputWithConsole(result);
+    public static void main(String[] args) throws FileNotFoundException {
+//        WithConsole console = new WithConsole();
+//        ArrayList<Double> coeffs = console.inputWithConsole();
+//        QuadraticSolver roots = new QuadraticSolver(coeffs);
+//        ArrayList<Double> result = roots.solve();
+//        console.outputWithConsole(result);
+
+        WithFile file = new WithFile();
+        try
+        {
+            ArrayList<ArrayList<Double>> all_coeffs = file.readFile(
+                    new FileReader("src/input.txt")
+            );
+            System.out.println("Прочитано: " + all_coeffs);
+
+            try (var writer = new FileWriter("src/output.txt"))
+            {
+                for (ArrayList<Double> coeffs:all_coeffs)
+                {
+                    QuadraticSolver solver = new QuadraticSolver(coeffs);
+                    ArrayList<Double> roots =solver.solve();
+
+                    file.writeFile(writer,roots);
+                    writer.write(System.lineSeparator());
+
+                    System.out.println("Корни уравнения " + coeffs + ": " + roots);
+                }
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
